6b355d50b7323cbc3726829bbbc24bae6242d65f
Leo Caldosa <caldosaleo@gmail.com>
2025-03-21 15:19:10 +0800

    print number of characters

--- count_characters.py
+++ count_characters.py
@@ -1,7 +1,5 @@
 # input name
 name = input("Enter full name: ")
-print(name)
 
-# count characters in name
-
-# print number of characters in name
+# print the count of characters in name
+print(len(name))
