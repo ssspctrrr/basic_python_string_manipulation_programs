f11d8571438436aa91764d13a452b7d820920eac
Leo Caldosa <caldosaleo@gmail.com>
2025-03-21 21:28:35 +0800

    get individual words

--- count_words.py
+++ count_words.py
@@ -3,6 +3,8 @@ sentence = input("Enter a full statement: ")
 print(sentence)
 
 # separate words in the sentence into words array
+words = sentence.split(" ")
+print(words)
 
 # count the elements in the words array
 
