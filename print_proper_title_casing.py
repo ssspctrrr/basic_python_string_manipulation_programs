532f3314593747b8047d5cfe1cca6e002058b247
Leo Caldosa <caldosaleo@gmail.com>
2025-03-21 14:38:18 +0800

    print proper title casing

--- print_proper_title_casing.py
+++ print_proper_title_casing.py
@@ -1,7 +1,5 @@
 # input name
 name = input("Enter full name (in incorrect casing): ")
-print(name)
 
-# put name in proper title casing
-
-# print name
+# print name in proper title casing
+print(name.title())
