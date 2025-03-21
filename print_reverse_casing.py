d88e81ac3ae00976c810aaa02b4c04dcdcaafa42
Leo Caldosa <caldosaleo@gmail.com>
2025-03-21 14:45:12 +0800

    print reverse casing

--- print_reverse_casing.py
+++ print_reverse_casing.py
@@ -1,7 +1,5 @@
 # input name
 name = input("Enter full name (in incorrect casing): ")
-print(name)
 
-# reverse casing in name
-
-# print name
+# print name in reverse casing
+print(name.swapcase())
